c3fa1854376df1107ad84da4473ab849249559c6
SALAHEDDINE AHANSAL <salaheddineahansal@gmail.com>
2023-04-29 02:02:45 +0100

    sidebar margin top

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    window.setTimeout(fadeout, 100);
+};
+function fadeout() {
+    document.querySelector('.preloader').style.opacity = '0';
+    document.querySelector('.preloader').style.display = 'none';
+}
